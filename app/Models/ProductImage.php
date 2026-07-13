857d5fda0e7cbd87f85a81daf390aa0606b8fc25
torangeshop <63396813+torangeshop@users.noreply.github.com>
2026-07-13 13:41:49 +0330

    Update ProductImage.php
    Signed-off-by: torangeshop <63396813+torangeshop@users.noreply.github.com>

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    protected $guarded = [];
+    protected $fillable=[
+        'product_id',
+        'image',
+        'sort'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
