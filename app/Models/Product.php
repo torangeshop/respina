84f0e619dbddd01d957e1c0f6b056346d015a3b7
torangeshop <63396813+torangeshop@users.noreply.github.com>
2026-07-13 13:38:32 +0330

    Update Product.php
    Signed-off-by: torangeshop <63396813+torangeshop@users.noreply.github.com>

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $guarded = [];
+    protected $fillable=[
+        'category_id',
+        'name',
+        'slug',
+        'short_description',
+        'description',
+        'base_price',
+        'thumbnail',
+        'featured',
+        'status'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
 }
